5e1338f719d9c74b18715f29a28d9c870ca20497
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2013-02-09 14:54:07 +1100

    Solve 9/2/2013 puzzle, better result string formatting.

--- symbolsum.py
+++ symbolsum.py
@@ -1,6 +1,15 @@
 from itertools import permutations
 import operator
 
+
+def operator_repr(op):
+    if op == operator.add:
+        return "+"
+    elif op == operator.sub:
+        return "-"
+    else:
+        return "?"
+
 class SymbolSumSolver(object):
     def __init__(self, symbols):
         self.symbols = symbols
@@ -35,17 +44,33 @@ class SymbolSumSolver(object):
             o2 = self._concat_symbols(operand2, solution)
             calculated_result = operator(o1, o2)
             
-            print "  %05d" % o1
-            print "- %05d" % o2
-            print " %6d" % calculated_result
-            print "Therefore: %s" % solution
+            result_str = """
+  {operand1}
+{operator} {operand2}
+{separator}
+  {result}
+
+{symbol_values} """
+
+            print result_str.format(
+                operand1=o1,
+                operand2=o2,
+                operator=operator_repr(operator),
+                separator='-' * 8,
+                result=calculated_result,
+                symbol_values=solution)
 
 if __name__ == "__main__":
 
-    all_symbols = ['star', 'circle', 'hexagon', 'plus', 'triangle']
-    operand1 = [ 'triangle', 'plus', 'plus', 'hexagon', 'hexagon' ]
-    operand2 = [  0,  'plus', 'star', 'circle', 'star' ]
-    result  = [ 'star', 'circle', 'hexagon', 'circle', 'star' ]
+    all_symbols = ['square', 'diamond', 'star', 'circle', 'hexagon', 'plus', 'triangle']
+
+    #operand1 = ['triangle', 'plus', 'plus', 'hexagon', 'hexagon']
+    #operand2 = [ 0,  'plus', 'star', 'circle', 'star']
+    #result  = ['star', 'circle', 'hexagon', 'circle', 'star']
+
+    operand1 = ['star', 'diamond', 'hexagon', 'square', 'triangle', 'circle']
+    operand2 = ['square',  'circle', 'hexagon', 'star', 'triangle', 'plus']
+    result  = ['circle', 'circle', 'plus', 'diamond', 'hexagon', 'star']
 
     solver = SymbolSumSolver(all_symbols)
-    solver.solve(operand1, operator.sub, operand2, result)
+    solver.solve(operand1, operator.add, operand2, result)
